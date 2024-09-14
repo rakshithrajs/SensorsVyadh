15bf2b79c684fbe0762e0272e6a4548154b39505
Rakshith Raj <rakshithrajr@gmail.com>
2024-09-14 22:24:20 +0530

    ho gaya lagtha tho hain testing karna hain

--- src/my_robot_controller/my_robot_controller/butt.py
+++ src/my_robot_controller/my_robot_controller/butt.py
@@ -10,37 +10,46 @@ class ButtonNode(Node):
     def __init__(self):
         super().__init__("button_node")
         self.subsa = self.create_subscription(String, "arduino_data", self.caller, 10)
-        global IR_sensor_data, US_sensor_data
-        IR_sensor_data = []
-        US_sensor_data = []
+        # global IR_sensor_data, US_sensor_data
+        # IR_sensor_data = []
+        # US_sensor_data = []
+        global sensors
+        sensors = {}
         self.sensor = self.create_service(Trigger, "get_avg_data", self.button_clicked)
 
     def average(self, list):
         return sum(list) / len(list)
 
     def caller(self, msg):
-        if (msg.data).split(":")[0] == "IR Reading":
-            IR_data = (msg.data).split(":")[1]
-            IR_sensor_data.append(float(IR_data))
+        # if (msg.data).split(":")[0] == "IR Reading":
+        #     IR_data = (msg.data).split(":")[1]
+        #     IR_sensor_data.append(float(IR_data))
+        # else:
+        #     US_data = (msg.data).split(":")[1]
+        #     US_sensor_data.append(float(US_data))
+        # if len(US_sensor_data) > 5:
+        #     US_sensor_data.pop(0)
+        # if len(IR_sensor_data) > 5:
+        #     IR_sensor_data.pop(0)
+        if (msg.data).split(":")[0] in list(sensors.keys()):
+            if len(sensors[(msg.data).split(":")[0]]['data']) >= 5:
+                sensors[(msg.data).split(":")[0]]['data'].pop(0)
+            sensors[(msg.data).split(":")[0]]['data'].append(float((msg.data).split(":")[1]))
         else:
-            US_data = (msg.data).split(":")[1]
-            US_sensor_data.append(float(US_data))
-        if len(US_sensor_data) > 5:
-            US_sensor_data.pop(0)
-        if len(IR_sensor_data) > 5:
-            IR_sensor_data.pop(0)
+            sensors[(msg.data).split(":")[0]] = {'data': [], 'avg': 0}
+        
 
     def button_clicked(self, response):
-        IR_avg = self.average(IR_sensor_data)
-        US_avg = self.average(US_sensor_data)
-        data = {"IR average": IR_avg, "US average": US_avg}
+        for i in sensors:
+            sensors[i]['avg'] = self.average(sensors[i]['data'])
+
         response.success = True
         with open("data.json", "w") as file:
-            json.dump(data, file)
+            json.dump(sensors, file)
         with open("data.csv", mode="w", newline="") as file:
             writer = csv.writer(file)
-            writer.writerow(data.keys())
-            writer.writerow(zip(data.values()))
+            writer.writerow(sensors.keys())
+            writer.writerow(zip(sensors.values()))
         self.get_logger().info("Button clicked")
         return response
 
